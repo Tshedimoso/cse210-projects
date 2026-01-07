eefb3670cf05a07f0da5cdb0862917ec5e8394b0
Tshedimoso <tsegona@byupathway.edu>
2026-01-07 14:55:32 +0200

    changes

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,6 +4,8 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        Console.Write("Hello World! This is the Exercise1 Project.");
     }
 }
+
+
