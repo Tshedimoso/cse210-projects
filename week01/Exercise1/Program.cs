812086ad7328cd634f94456b2a17acbf61ab2d2b
Tshedimoso <tsegona@byupathway.edu>
2026-01-07 15:26:37 +0200

    name and last name

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,7 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.Write("Hello World! This is the Exercise1 Project.");
+        Console.WriteLine("What is your first name?");
+        string fname = Console.ReadLine();
+
+        Console.WriteLine("What is your last name?");
+        string lname = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {lname}, {fname} {lname}");
+
     }
 }
 
