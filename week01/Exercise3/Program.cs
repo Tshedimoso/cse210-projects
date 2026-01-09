ff250ce5029cf778c147eea49259cca0ff6f23f7
Tshedimoso <tsegona@byupathway.edu>
2026-01-09 17:34:50 +0200

    firtst step of excersice 3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Console.Write("What is the magic number? ");
+        string userInput = Console.ReadLine();
+        int randomNumber = int.Parse(userInput);
+
+        Console.Write("What is your guess? ");
+        string userGuess = Console.ReadLine();
+        int guess = int.Parse(userGuess);
+
+        if (guess == randomNumber)
+        {
+            Console.WriteLine("You guessed it!");
+        }
+
+        else if (guess >= randomNumber)
+        {
+            Console.WriteLine("Lower");
+        }
+
+        else
+        {
+            Console.WriteLine("Higher");
+        }
+
+
+
     }
 }
