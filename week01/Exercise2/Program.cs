766037df6256f3639d79eaba5c65b08c2bf21765
Tshedimoso <tsegona@byupathway.edu>
2026-01-09 16:37:30 +0200

    changes

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,50 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        
+        Console.Write("Please enter your grade percentage: ");
+        string gradePercentage = Console.ReadLine();
+        int number = int.Parse(gradePercentage);
+        string letter = "";
+
+        if (number >= 90)
+        {
+             letter = "A";
+        }
+
+        else if (number >= 80)
+        {
+             letter = "B";
+        }
+
+        else if (number >= 70)
+        {
+             letter = "C";
+        }
+
+        else if (number >= 60)
+        {
+             letter = "D";
+        }
+
+        else 
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine(letter);
+
+        
+
+        if (number >= 70)
+        {
+            Console.Write("Congradulations, you passed the course!");
+        }
+
+        else
+        {
+            Console.Write("Sorry, you failed the course. Try again next block.");
+        }
+
     }
 }
