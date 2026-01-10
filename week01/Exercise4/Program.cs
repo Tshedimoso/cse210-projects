037638769a56cbefb05a60da0ce9b8a1fdc4ca2f
Tshedimoso <tsegona@byupathway.edu>
2026-01-10 22:41:37 +0200

    part one

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,29 @@
 using System;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        List<int> numbers = new List<int>();
+
+        int userInput = -1;
+
+        while (userInput != 0)
+        {
+            Console.Write("Enter a list of numbers, type 0 when finished: ");
+
+            string response = Console.ReadLine() ;
+            userInput = int.Parse(response);
+
+            if (userInput != 0)
+            {
+                numbers.Add(userInput);
+            }
+            
+        }
+       
+        
+
     }
 }
