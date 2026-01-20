def4de98a82137072fda0044298ee4bf977e5cc4
Tshedimoso <tsegona@byupathway.edu>
2026-01-20 15:15:06 +0200

    instances

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        Console.WriteLine(job1._jobTitle);
+
+        Job job2 = new Job();
+        job2._jobTitle = "Apple";
+        Console.WriteLine(job2._jobTitle);
     }
+
 }
--- week02/Resumes/job.cs
+++ week02/Resumes/job.cs
@@ -4,4 +4,9 @@
         // Behaviors: Display the job information in the format "Job Title (Company) StartYear-EndYear".
         // Example: "Software Engineer (Microsoft) 2019-2022".
 
+        public string _company;
+        public string _jobTitle;
+        public int _startYear;
+        public int _endYear;
+
     }
