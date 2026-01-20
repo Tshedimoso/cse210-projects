87c6e00f25d237eb528130c79ae8b760c662a586
Tshedimoso <tsegona@byupathway.edu>
2026-01-20 18:31:26 +0200

    Changes

--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -1,9 +1,45 @@
 using System;
+using System.Xml.Serialization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Journal Project.");
+        Console.WriteLine("Welcome to the Journal Program! ");
+
+        Console.WriteLine("Please select one of the following choices: ");
+        Console.WriteLine("1. Write");
+        Console.WriteLine("2. Display");
+        Console.WriteLine("3. Load");
+        Console.WriteLine("4. Save");
+
+
+        string choice = "";
+
+        if (choice == "1" )
+        {
+            
+        }
+
+        else if (choice == "2")
+        {
+            
+        }
+
+         else if (choice == "3")
+        {
+            
+        }
+
+         else if (choice == "5")
+        {
+            
+        }
+
+        else
+        {
+            
+        }
+
     }
 }
