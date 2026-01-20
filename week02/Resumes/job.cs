c4eede760ca8a634521f00140b4cd116caa47376
Tshedimoso <tsegona@byupathway.edu>
2026-01-20 14:59:24 +0200

    Classes

---
+++ week02/Resumes/job.cs
@@ -0,0 +1,7 @@
+ public class Job
+    {
+        // Resposibilities: Keeps track of the company, job title, start year, and end year.
+        // Behaviors: Display the job information in the format "Job Title (Company) StartYear-EndYear".
+        // Example: "Software Engineer (Microsoft) 2019-2022".
+
+    }
---
+++ week02/Resumes/resume.cs
@@ -0,0 +1,6 @@
+ public class Resume
+    {
+        // Resposibilities: Keep track of the persons name and a list of their jobs. 
+        // Behaviors: Display the Resume, which shows the name first, followed by displaying each one of their jobs.
+        
+    }
