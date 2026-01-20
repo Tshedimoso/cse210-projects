c9015568807cf3860d0e223b6bfba96a0d4c523d
Tshedimoso <tsegona@byupathway.edu>
2026-01-20 18:08:15 +0200

    Final

--- week02/Resumes/resume.cs
+++ week02/Resumes/resume.cs
@@ -2,5 +2,20 @@
     {
         // Resposibilities: Keep track of the persons name and a list of their jobs. 
         // Behaviors: Display the Resume, which shows the name first, followed by displaying each one of their jobs.
+      public string _name;
+      public List<Job> _jobs = new List<Job>();
+
+      public void DisplayJobDetails()
+    {
+        Console.WriteLine($"Name : {_name}");
+        Console.WriteLine("Jobs:");
+        foreach (Job job in _jobs)
+        {
+           job.DisplayJobDetails();
+        }
         
+    
+    
+    }
+
     }
