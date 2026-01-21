ae542f19ac366daad6014c0e77810a74a1070f58
Tshedimoso <tsegona@byupathway.edu>
2026-01-21 09:28:40 +0200

    While loop

--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -5,40 +5,47 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Welcome to the Journal Program! ");
+        Console.WriteLine("Welcome to the Journal Program!");
+        int choice = -1;
 
-        Console.WriteLine("Please select one of the following choices: ");
-        Console.WriteLine("1. Write");
-        Console.WriteLine("2. Display");
-        Console.WriteLine("3. Load");
-        Console.WriteLine("4. Save");
-
-
-        string choice = "";
-
-        if (choice == "1" )
+        while (choice != 5)
         {
-            
-        }
+            Console.WriteLine("Please select one of the following choices:");
+            Console.WriteLine("1. Write");
+            Console.WriteLine("2. Display");
+            Console.WriteLine("3. Load");
+            Console.WriteLine("4. Save");
+            Console.WriteLine("5. Quit");
+            Console.Write("What would you like to do? ");
+            string userChoice = Console.ReadLine();
+            choice = int.Parse(userChoice);
 
-        else if (choice == "2")
-        {
+            if (choice == 1)
+            {
+                Console.WriteLine("Hooray!");
+            }
             
-        }
+            else if (choice == 2)
+            {
+                Console.WriteLine("Yay!");
+            }
 
-         else if (choice == "3")
-        {
-            
-        }
+            else if (choice == 3)
+            {
+                Console.WriteLine("Good!");
+            }
 
-         else if (choice == "5")
-        {
-            
-        }
+            else if (choice == 4)
+            {
+                Console.WriteLine("Very Good!");
+            }
 
-        else
-        {
-            
+            else
+            {
+                
+            }
+
+
         }
 
     }
