370529a0c9a4a9dc27e8ef7468b9c24f8d025fa1
Tshedimoso <tsegona@byupathway.edu>
2026-02-06 22:28:07 +0200

    changes

--- week04/OnlineOrdering/Product.cs
+++ week04/OnlineOrdering/Product.cs
@@ -4,4 +4,13 @@ public class Product
     private string _productId;
     private double _price;
     private int _quantity;
+
+    public Product(string name, string productId, double price, int quantity)
+    {
+        _name = name ;
+        _productId = productId;
+        _price = price;
+        _quantity = quantity;
+    }
+
 }
